b476c12b17a2738254ab75d370bb03f995c4c3e8
Louis Pallett <louis.pallett@outlook.com>
2024-04-16 12:53:28 +0100

    Create sign in pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.pug"],
+  content: [
+    "./views/*.pug",
+    "./public/javascripts/*.js",
+  ],
   theme: {
     extend: {},
   },
