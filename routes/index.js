ad99cf874da21ba808cb248f75adeb44474e6f92
Louis Pallett <louis.pallett@outlook.com>
2024-04-16 14:11:33 +0100

    Add checkboxes for tournaments

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/sign-up');
+  res.redirect('/users/sign-up');
 });
 
 module.exports = router;
