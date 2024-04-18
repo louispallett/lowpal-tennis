17ccc622a7be19ff74366c0ecdd21ce9d9329391
Louis Pallett <louis.pallett@outlook.com>
2024-04-18 10:18:42 +0100

    Revert back to old file structure

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/sign-up', (req, res, next) => {
-  res.render('user-signing/sign-up', { title: 'Sign Up' });
+  res.render('sign-up', { title: 'Sign Up' });
 });
 
 router.get("/sign-in", (req, res, next) => {
-  res.render("user-signing/sign-in", { title: "Sign In" });
+  res.render("sign-in", { title: "Sign In" });
 })
 
 module.exports = router;
