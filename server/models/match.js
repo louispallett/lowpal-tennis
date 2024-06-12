2f1560d8ca03cf18858b521cde50bb648550f360
Louis Pallett <louis.pallett@outlook.com>
2024-06-12 13:27:29 +0100

    Update schema elements

--- server/models/match.js
+++ server/models/match.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const Match = new Schema({
     category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
@@ -8,16 +9,21 @@ const Match = new Schema({
     // Scores can possibly be formatted at backend?
     p1Score: { type: String }, 
     p2Score: { type: String }, 
+    date: { type: Date, required: true }
 });
 
 Match.virtual("url").get(function() {
     return `/catalog/item/${this._id}`;
 });
 
-Match.pre('validate', function(next) {
-    if (this.todoList.length > 2) throw("todoList exceeds maximum array size (10)!");
-    next();
+Match.virtual("date_formatted").get(function() {
+    return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATETIME_MED);
 });
+
+// Match.pre('validate', function(next) {
+//     if (this.todoList.length > 2) throw("todoList exceeds maximum array size (10)!");
+//     next();
+// });
 
 Match.set('toJSON', { virtuals: true });
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const User = new Schema(
-    {
+const User = new Schema({
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
         email: { type: String, required: true },
         male: { type: Boolean, required: true },
-        categories: [{ type: String, required: true }], // We may want to reference this as a schema type
+        categories: { type: Array, required: true }, // We may want to reference this as a schema type
+        seeded: { type: Boolean, required: true },
         password: { type: String, required: true },
+        mobile: { type: String, required: true },
         host: { type: Boolean },
     }
 );
@@ -16,5 +17,7 @@ const User = new Schema(
 User.virtual("name").get(function() {
     return(`${this.firstName} ${this.lastName}`);
 });
+
+User.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model("User", User);
