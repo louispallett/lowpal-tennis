818b97dedc728c407a200216d7bf534ab4e9483e
Louis Pallett <louis.pallett@outlook.com>
2024-06-12 16:49:45 +0100

    Create basic controllers

---
+++ server/Controllers/bracketController.js
@@ -0,0 +1,10 @@
+const asyncHandler = require("express-async-handler");
+
+
+exports.get_bracket = asyncHandler((req, res, next) => {
+
+});
+
+exports.get_match_detail = asyncHandler((req, res, next) => {
+
+});
---
+++ server/Controllers/dashboardController.js
@@ -0,0 +1,17 @@
+const asyncHandler = require("express-async-handler");
+
+exports.get_categories = asyncHandler((req, res, next) => {
+
+});
+
+exports.get_category_detail = asyncHandler((req, res, next) => {
+
+});
+
+exports.get_match_detail = asyncHandler((req, res, next) => {
+
+});
+
+exports.post_match_results = asyncHandler((req, res, next) => {
+
+});
