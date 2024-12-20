b123c470e7cbb01cb78dfbf4c4839358175c0a21
Louis Pallett <louis.pallett@outlook.com>
2024-12-20 23:33:16 +0000

    Move all non HTTP request functions to new userAuxillary file

--- server/Controllers/usersController.js
+++ server/Controllers/usersController.js
@@ -30,10 +30,10 @@ const jwt = require("jsonwebtoken")
 const passport = require("../config/passport");
 const { body, validationResult } = require("express-validator");
 
-const transporter = require("../config/nodemailerConfig");
 const verifyUser = require("../config/verifyUser");
 const User = require("../models/user");
-const Category = require("../models/category");
+
+const { sendConfirmationEmail, addUserToCategories, sendResetPasswordEmail, sendUpdateEmailEmail, sendUpdatePasswordEmail } = require("../public/scripts/userAuxillary");
 
 exports.signIn = [
     body("email", "Email needs to be a valid email")
@@ -129,43 +129,6 @@ exports.signUp = [
     })
 ];
 
-const sendConfirmationEmail = (user, categoryNames) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: user.email,
-        subject: "2024 Saltford In-House Tennis Tournament",
-        text: `Hi ${user.firstName}, 
-            \nThanks you for signing up to the 2024 Saltford In-House Tournament. This email confirms that your sign up in the following categories:
-            \n${categoryNames}
-            \nTeams and matches will be announced on Friday 26th July. Until then, please feel free to reply to this email with any questions you have.
-            \nBest wishes,\nLouis`
-    }
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error)
-            // TODO: Throw a new error here to trigger the catch error
-            // throw error...
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    })
-}
-
-const addUserToCategories = async (userId, userCategories) => {
-    try {
-        for (const userCategory of userCategories) {
-            await Category.findByIdAndUpdate(
-                userCategory,
-                { $push: { players: userId } },
-                { new: true, useFindAndModify: false } // Ensures we get the updated document back and avoid deprecation warnings
-            );
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
     try {
         let user = await User.find({ email: req.body.email });
@@ -205,28 +168,6 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     }
 });
 
-const sendResetPasswordEmail = (user, newPassword) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: user.email,
-        subject: "2024 Saltford In-House Tennis Tournament",
-        text: `Hi ${user.firstName}, 
-            \nThanks you for requesting to reset your password.
-            \nYour password has been reset to: ${newPassword}
-            \nIf you wish to change your password, please login to your account with your new password and navigate to the account settings via the site menu.`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error)
-            // TODO: Throw a new error here to trigger the catch error
-            // throw error...
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
-}
-
 exports.updateName = [
     body("firstName")
     .trim()
@@ -261,7 +202,7 @@ exports.updateName = [
             res.sendStatus(200);
         } catch (err) {
             console.log(err);
-
+            res.status(500).json({ message: err });
         }
     })
 ];
@@ -296,36 +237,10 @@ exports.updateEmail = [
             res.sendStatus(200);
         } catch (err) {
             console.log(err);
-
+            res.status(500).json({ message: err });
         }
     })
 ];
-
-// Note that we have to pass in the req.body.email here because the user.email will 
-// be the old one (since the fetch request to the database was made before updating
-// the email there)
-const sendUpdateEmailEmail = (user, newEmail) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: newEmail,
-        subject: "LowPal Tennis: Email Updated",
-        text: `Hi ${user.firstName},
-            \nThank you for requesting a change to your email address. This email confirms that you have updated your address to ${newEmail}.
-            \nIf you did not make this request, please respond to this email immediately and let us know.
-            \nBest wishes,
-            Louis`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            // TODO: Throw a new error here to trigger the catch error
-            // throw error...
-        } else {
-            console.log("Email send: " + info.response);
-        }
-    })
-}
 
 exports.updateMob = [
     body("mobile")
@@ -358,7 +273,7 @@ exports.updateMob = [
 
         } catch (err) {
             console.log(err);
-
+            res.status(500).json({ message: err });
         }
     })
 ];
@@ -402,25 +317,6 @@ exports.updatePassword = [
 
     })
 ];
-
-const sendUpdatePasswordEmail = (user) => {
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: user.email,
-        subject: "LowPal Tennis: Reset password confirmation",
-        text: `Hi ${user.firstName},
-            \nYou are receieving this email because a successful request was made to reset your password.
-            \nIf you did not make this request, please respond this to email immediately.`
-    }
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
-}
 
 exports.verify = asyncHandler(async (req, res, next) => {
     try {
