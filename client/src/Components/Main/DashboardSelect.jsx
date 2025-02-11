f93aa3e0193f007cf88da2c36aea9f7b9920cc8a
Louis Pallett <louis.pallett@outlook.com>
2025-02-11 21:58:49 +0000

    Update component to display hosting and playing tournaments from user details. Remove lodash import

--- client/src/Components/Main/DashboardSelect.jsx
+++ client/src/Components/Main/DashboardSelect.jsx
@@ -4,6 +4,7 @@ import _ from "lodash";
 import { useEffect, useState } from "react";
 import racketRed from "/assets/images/racket-red.svg";
 import racketBlue from "/assets/images/racket-blue.svg";
+import { Link } from "react-router-dom";
 
 export default function TournamentSelect() {
     const [userInfo, setUserInfo] = useState(false);
@@ -28,6 +29,9 @@ export default function TournamentSelect() {
         getuserInfo();
     }, []);
 
+    let isHosting;
+    let isPlaying;
+
     if (userInfo) {
         tournamentsPlaying = {
             signUps: userInfo.userTournamentsPlaying.filter(x => x.stage === "sign-up"),
@@ -40,7 +44,8 @@ export default function TournamentSelect() {
             actives: userInfo.userTournamentsHosting.filter(x => x.stage === "play"),
             finished: userInfo.userTournamentsHosting.filter(x => x.stage === "finished")
         }
-        console.log(tournamentsHosting);
+        isHosting = tournamentsHosting.signUps.length > 0 || tournamentsHosting.actives.length > 0 || tournamentsHosting.finished.length > 0;
+        isPlaying = tournamentsPlaying.signUps.length > 0 || tournamentsPlaying.actives.length > 0 || tournamentsPlaying.finished.length > 0;    
     }
 
     return (
@@ -52,7 +57,7 @@ export default function TournamentSelect() {
             </div>
             { userInfo ? (
                 <>
-                    { !_.isEmpty(tournamentsHosting) && (
+                    { isHosting && (
                         <>
                             <div className="mt-8 form-input bg-indigo-500 text-white">
                                 <h4 className="italic">Hosting tournaments</h4>
@@ -60,6 +65,48 @@ export default function TournamentSelect() {
                             { tournamentsHosting.signUps.length > 0 && (
                                 <div className="tournament-grid">
                                     { tournamentsHosting.signUps.map((item) => (
+                                        <UserTournament data={item} key={item._id} />
+                                    ))}
+                                </div>
+                            )}
+                            { tournamentsHosting.actives.length > 0 && (
+                                <div className="tournament-grid">
+                                    { tournamentsHosting.actives.map((item) => (
+                                        <UserTournament data={item} key={item._id} />
+                                    ))}
+                                </div>
+                            )}
+                            { tournamentsHosting.finished.length > 0 && (
+                                <div className="tournament-grid">
+                                    { tournamentsHosting.finished.map((item) => (
+                                        <UserTournament data={item} key={item._id} />
+                                    ))}
+                                </div>
+                            )}
+                        </>
+                    )}
+                    { isPlaying && (
+                        <>
+                            <div className="mt-8 form-input bg-indigo-500 text-white">
+                                <h4 className="italic">Hosting tournaments</h4>
+                            </div>
+                            { tournamentsPlaying.signUps.length > 0 && (
+                                <div className="tournament-grid">
+                                    { tournamentsPlaying.signUps.map((item) => (
+                                        <UserTournament data={item} key={item._id} />
+                                    ))}
+                                </div>
+                            )}
+                            { tournamentsPlaying.actives.length > 0 && (
+                                <div className="tournament-grid">
+                                    { tournamentsPlaying.actives.map((item) => (
+                                        <UserTournament data={item} key={item._id} />
+                                    ))}
+                                </div>
+                            )}
+                            { tournamentsPlaying.finished.length > 0 && (
+                                <div className="tournament-grid">
+                                    { tournamentsPlaying.finished.map((item) => (
                                         <UserTournament data={item} key={item._id} />
                                     ))}
                                 </div>
@@ -107,17 +154,17 @@ function UserTournament({ data }) {
     console.log(tournamentInfo);
 
     return (
-        <div className="form-input bg-lime-500">
+        <Link to={data._id} className="form-input bg-lime-500 hover:bg-lime-400">
             <h3>{data.name}</h3>
             <div className="tournament-grid-sm">
                 <p className="form-input bg-indigo-500 text-white">Host: {data.host["name-long"]}</p>
-                <p className="form-input bg-indigo-500 text-white">Stage: {data.stage}</p>
-                <p className="form-input bg-indigo-500 text-white">Number of players</p>
-                <p className="form-input bg-indigo-500 text-white">Total Matches</p>
-                <p className="form-input bg-indigo-500 text-white">Active matches</p>
+                <p className="form-input bg-indigo-500 text-white">Stage: <i>{data.stage}</i></p>
+                <p className="form-input bg-indigo-500 text-white">Number of players: {tournamentInfo.nOfPlayers}</p>
+                <p className="form-input bg-indigo-500 text-white">Total Matches: {tournamentInfo.nOfMatches}</p>
+                <p className="form-input bg-indigo-500 text-white">Active matches: {tournamentInfo.nOfActiveMatches}</p>
                 <p className="form-input bg-indigo-500 text-white">Date Created: {data.startDateFormatted}</p>
             </div>
-        </div>
+        </Link>
     )
 }
 
