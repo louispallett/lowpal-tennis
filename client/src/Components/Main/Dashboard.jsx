b9474ddf17b4e1ae8ee5a2c0401a8d9a0d03d823
louispallett <louis.pallett@outlook.com>
2025-04-23 12:45:28 +0100

    Update styling of tournament brackets to simple, custom design.

--- client/src/Components/Main/Dashboard.jsx
+++ client/src/Components/Main/Dashboard.jsx
@@ -45,7 +45,7 @@ export default function Dashboard() {
                     <></>
                 ))}
             </div>
-            <div className="flex flex-col gap-5 mx-1 sm:mx-1.5 lg:mx-5">
+            <div className="flex flex-col gap-5 sm:mx-1.5 lg:mx-5">
                 { data && (
                     <>
                         <TournamentInfo data={data} />
@@ -69,15 +69,15 @@ export default function Dashboard() {
 
 function TournamentInfo({ data }) {
     return (
-        <div className="form-input bg-lime-400">
+        <div className="standard-container bg-lime-400">
             <h3>{data.tournament.name}</h3>
             <div className="tournament-grid-sm">
-                <p className="form-input bg-indigo-500 text-white">Code: {data.tournament.tournamentCode}</p>
-                <p className="form-input bg-indigo-500 text-white">Host: {data.tournament.host["name-long"]}</p>
-                <p className="form-input bg-indigo-500 text-white">Stage: <i>{data.tournament.stage}</i></p>
-                <p className="form-input bg-indigo-500 text-white">Date Created: {data.tournament.startDateFormatted}</p>
-                <p className="form-input bg-indigo-500 text-white">Number of players: {data.allPlayers}</p>
-                <p className="form-input bg-indigo-500 text-white">Number of active matches: {data.tournamentMatches.filter(match => match.state === "SCHEDULED").length}</p>
+                <p className="standard-container bg-indigo-500 text-white">Code: {data.tournament.tournamentCode}</p>
+                <p className="standard-container bg-indigo-500 text-white">Host: {data.tournament.host["name-long"]}</p>
+                <p className="standard-container bg-indigo-500 text-white">Stage: <i>{data.tournament.stage}</i></p>
+                <p className="standard-container bg-indigo-500 text-white">Date Created: {data.tournament.startDateFormatted}</p>
+                <p className="standard-container bg-indigo-500 text-white">Number of players: {data.allPlayers}</p>
+                <p className="standard-container bg-indigo-500 text-white">Number of active matches: {data.tournamentMatches.filter(match => match.state === "SCHEDULED" && match.participants.length > 1).length}</p>
             </div>
         </div>
     )
@@ -85,17 +85,17 @@ function TournamentInfo({ data }) {
 
 function UserTeams({ teams }) {
     return (
-        <div className="form-input bg-slate-100 flex flex-col gap-2.5 z-0">
+        <div className="standard-container bg-slate-100 flex flex-col gap-2.5 z-0">
             <h3>Your Teams</h3>
             { teams.length > 0 ? (
                 <div className="tournament-grid-sm">
                     {teams.map(info => (
-                        <div className="form-input bg-indigo-600 text-white max-w-4xl">
-                            <h5 className="form-input mb-2.5 text-center bg-lime-400 shadow-none">{info.category.name}</h5>
+                        <div className="standard-container bg-indigo-600 text-white max-w-4xl">
+                            <h5 className="standard-container mb-2.5 text-center bg-lime-400 shadow-none">{info.category.name}</h5>
                             <div className="flex justify-between flex-col lg:flex-row items-center gap-2.5">
-                                <p className="form-input bg-slate-100 shadow-none text-right">{info.players[0].user.firstName} {info.players[0].user.lastName}</p>
+                                <p className="standard-container bg-slate-100 shadow-none text-right">{info.players[0].user.firstName} {info.players[0].user.lastName}</p>
                                 <p>and</p>
-                                <p className="form-input bg-slate-100 shadow-none">{info.players[1].user.firstName} {info.players[1].user.lastName}</p>
+                                <p className="standard-container bg-slate-100 shadow-none">{info.players[1].user.firstName} {info.players[1].user.lastName}</p>
                             </div>
                         </div>
                     ))}
@@ -138,7 +138,7 @@ function UserMatches() {
     }, [])
 
     return (
-        <div className="form-input bg-slate-100 flex flex-col gap-2.5 z-0">
+        <div className="standard-container bg-slate-100 flex flex-col gap-2.5 z-0">
             <h3>Your Upcoming Matches</h3>
             { matches && (
                 <>
@@ -166,16 +166,16 @@ function UserMatches() {
 function MatchCard({ data }) {
     return (
         <Link to={"match/" + data._id}>
-            <div className="form-input bg-indigo-600 text-white max-w-4xl hover:bg-indigo-500">
-                <h5 className="form-input mb-2.5 text-center bg-lime-400 shadow-none">{data.category.name}</h5>
+            <div className="standard-container h-full bg-indigo-600 text-white max-w-4xl hover:bg-indigo-500">
+                <h5 className="standard-container mb-2.5 text-center bg-lime-400 shadow-none">{data.category.name}</h5>
                 <div className="flex justify-between flex-col lg:flex-row items-center gap-2.5">
-                    <p className="form-input bg-slate-100 shadow-none">{data.participants.length > 0 ? data.participants[0].name : "TBC"}</p>
+                    <p className="standard-container bg-slate-100 shadow-none">{data.participants.length > 0 ? data.participants[0].name : "TBC"}</p>
                     <p>vs</p>
-                    <p className="form-input bg-slate-100 shadow-none">{data.participants.length > 1 ? data.participants[1].name : "TBC"}</p>
+                    <p className="standard-container bg-slate-100 shadow-none">{data.participants.length > 1 ? data.participants[1].name : "TBC"}</p>
                 </div>
                 <div className="flex gap-2.5">
-                    <p className="form-input bg-slate-100 shadow-none mt-2.5">Deadline: {data.deadline}</p>
-                    <p className="form-input bg-slate-100 shadow-none mt-2.5">Round: {data.tournamentRoundText}</p>
+                    <p className="standard-container bg-slate-100 shadow-none mt-2.5">Deadline: {data.deadline}</p>
+                    <p className="standard-container bg-slate-100 shadow-none mt-2.5">Round: {data.tournamentRoundText}</p>
                 </div>
             </div>
         </Link>
@@ -217,10 +217,10 @@ function TournamentResults() {
         getMatches();
     }, [])
     return (
-        <div className="form-input bg-slate-100 flex flex-col gap-2.5 z-0">
+        <div className="standard-container bg-slate-100 flex flex-col gap-2.5 z-0">
             <h3>Tournament Results</h3>
             { matches && (
-                <>
+                <div className="flex flex-col items-center gap-2.5">
                     { matches.length > 0 ? (
                         <>
                             { groupedMatches.map(item => (
@@ -236,7 +236,7 @@ function TournamentResults() {
                             <p>Matches for this tournament haven't been created yet!</p>
                         </div>
                     )}
-                </>
+                </div>
             )}
             <div className="flex justify-center">
                 { loading && (
