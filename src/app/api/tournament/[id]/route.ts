2d96da97422f16e02fb0eb5af9ac1c35ec41e4fd
Louis Nicholson-Pallett <louis.pallett@outlook.com>
2025-12-01 12:59:47 +0000

    bug(issue_21): add connectToDB() to PUT request for updating tournament stage. (#22)

--- src/app/api/tournament/[id]/route.ts
+++ src/app/api/tournament/[id]/route.ts
@@ -1,3 +1,4 @@
+import { connectToDB } from "@/lib/db";
 import HttpError from "@/lib/HttpError";
 import { MatchType } from "@/lib/types";
 import Match from "@/models/Match";
@@ -7,52 +8,60 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const PutValidation = z.object({
-    tournamentId: z.string().trim().refine(
-        (v) => mongoose.Types.ObjectId.isValid(v), {
-            message: "Invalid tournament ID"
-        }
-    )
+  tournamentId: z
+    .string()
+    .trim()
+    .refine((v) => mongoose.Types.ObjectId.isValid(v), {
+      message: "Invalid tournament ID",
+    }),
 });
 
 const tournamentStages = ["sign-up", "draw", "play", "finished"];
 
-const checkPlayInvalid = (matches:MatchType[]):boolean => {
-    const activeMatches = matches.filter(v => v.state === "SCHEDULED");
-    return activeMatches.length > 0;
-}
+const checkPlayInvalid = (matches: MatchType[]): boolean => {
+  const activeMatches = matches.filter((v) => v.state === "SCHEDULED");
+  return activeMatches.length > 0;
+};
 
 export async function PUT(
-    req:NextRequest,
-    { params }: { params: { id:string }}
+  req: NextRequest,
+  { params }: { params: { id: string } },
 ) {
-    try {
-        const { id } = await params;
-        const parsed = PutValidation.safeParse({ tournamentId: id });
-        if (!parsed.success) throw new HttpError(parsed.error.message, 400);
+  try {
+    await connectToDB();
 
-        const { tournamentId } = parsed.data;
-        const tournament = await Tournament.findById(tournamentId);
-        const stageIndex = tournamentStages.indexOf(tournament.stage);
+    const { id } = await params;
+    const parsed = PutValidation.safeParse({ tournamentId: id });
+    if (!parsed.success) throw new HttpError(parsed.error.message, 400);
 
-        if (tournament.stage === "play") {
-            const matches = await Match.find({ tournament: tournamentId });
-            if (checkPlayInvalid(matches)) {
-                throw new HttpError("There are still active matches in this tournament", 200);
-            }
-        }
+    const { tournamentId } = parsed.data;
+    const tournament = await Tournament.findById(tournamentId);
+    const stageIndex = tournamentStages.indexOf(tournament.stage);
 
-        if (!tournamentStages[stageIndex + 1]) throw new HttpError("Stage out of bounds", 400);
-
-        await Tournament.updateOne(
-            { _id: tournamentId },
-            { stage: tournamentStages[stageIndex + 1] }
+    if (tournament.stage === "play") {
+      const matches = await Match.find({ tournament: tournamentId });
+      if (checkPlayInvalid(matches)) {
+        throw new HttpError(
+          "There are still active matches in this tournament",
+          200,
         );
-
-        return new NextResponse(null, { status: 204 });
-    } catch (err:any) {
-        return NextResponse.json(
-            { success: false, message: err.message || "Internal Server Error" },
-            { status: err.statusCode || 500 }
-        );
+      }
     }
+
+    if (!tournamentStages[stageIndex + 1])
+      throw new HttpError("Stage out of bounds", 400);
+
+    await Tournament.updateOne(
+      { _id: tournamentId },
+      { stage: tournamentStages[stageIndex + 1] },
+    );
+
+    return new NextResponse(null, { status: 204 });
+  } catch (err: any) {
+    return NextResponse.json(
+      { success: false, message: err.message || "Internal Server Error" },
+      { status: err.statusCode || 500 },
+    );
+  }
 }
+
