83680c348409093bf73e3bd0bc811aac3064b31e
Louis Nicholson-Pallett <louis.pallett@outlook.com>
2025-12-01 15:07:30 +0000

    Test - Issue 23, Skipping tests (#24)
    * test(auth_register test skip): Wrap nodemailer request in conditional to stop it during testing.
    
    * test(auth_register test skip): Remove skips.

--- src/__tests__/auth_register.test.ts
+++ src/__tests__/auth_register.test.ts
@@ -1,63 +1,63 @@
 import { POST } from "@/app/api/auth/register/route";
 
 describe("API for User model", () => {
-    it.skip("Creates a user", async () => {
-        const req = new Request("http://localhost/api/auth/register", {
-            method: "POST",
-            body: JSON.stringify({
-                firstName: "John",
-                lastName: "Doe",
-                email: "John.Doe@example.com",
-                mobCode: "+44",
-                mobile: "6701564720", // randomly generated...
-                password: "HelloWorld1!",
-            }),
-            headers: { "Content-Type": "application/json" }
-        });
-
-        const res = await POST(req);
-        const json = await res.json();
-
-        expect(res.status).toBe(201);
-        expect(json.firstName).toBe("John");
+  it("Creates a user", async () => {
+    const req = new Request("http://localhost/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: "John",
+        lastName: "Doe",
+        email: "John.Doe@example.com",
+        mobCode: "+44",
+        mobile: "6701564720", // randomly generated...
+        password: "HelloWorld1!",
+      }),
+      headers: { "Content-Type": "application/json" },
     });
 
-    it.skip("Trims data successfully", async () => {
-        const req = new Request("http://localhost/api/auth/register", {
-            method: "POST",
-            body: JSON.stringify({
-                firstName: "John",
-                lastName: "Doe",
-                email: " John.Doe@example.com ",
-                mobCode: "+44",
-                mobile: "6701564720",
-                password: "HelloWorld1!",
-            }),
-            headers: { "Content-Type": "application/json" }
-        });
+    const res = await POST(req);
+    const json = await res.json();
 
-        const res = await POST(req);
-        const json = await res.json();
+    expect(res.status).toBe(201);
+    expect(json.firstName).toBe("John");
+  });
 
-        expect(res.status).toBe(201);
-        expect(json.email).toBe("john.doe@example.com");
+  it("Trims data successfully", async () => {
+    const req = new Request("http://localhost/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: "John",
+        lastName: "Doe",
+        email: " John.Doe@example.com ",
+        mobCode: "+44",
+        mobile: "6701564720",
+        password: "HelloWorld1!",
+      }),
+      headers: { "Content-Type": "application/json" },
     });
 
-    it("Returns error for weak passwords", async () => {
-        const req = new Request("http://localhost/api/auth/register", {
-            method: "POST",
-            body: JSON.stringify({
-                firstName: "John",
-                lastName: "Doe",
-                email: "John.Doe@example.com",
-                password: "HelloWorld1",
-                passkey: process.env.PASS_KEY
-            }),
-            headers: { "Content-Type": "application/json" }
-        });
+    const res = await POST(req);
+    const json = await res.json();
 
-        const res = await POST(req);
+    expect(res.status).toBe(201);
+    expect(json.email).toBe("john.doe@example.com");
+  });
 
-        expect(res.status).toBe(400);
-    })
+  it("Returns error for weak passwords", async () => {
+    const req = new Request("http://localhost/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: "John",
+        lastName: "Doe",
+        email: "John.Doe@example.com",
+        password: "HelloWorld1",
+        passkey: process.env.PASS_KEY,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const res = await POST(req);
+
+    expect(res.status).toBe(400);
+  });
 });
