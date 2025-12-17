82f718b7c8c7f5fe5caba1ada7514b8445b88c3d
louispallett <louis.pallett@outlook.com>
2025-12-17 22:06:07 +0000

    refactor(bracket-component): prettier refactor.

--- src/app/dashboard/[tournamentId]/TournamentResults.jsx
+++ src/app/dashboard/[tournamentId]/TournamentResults.jsx
@@ -1,55 +1,60 @@
-"use client"
+"use client";
 
 import Bracket from "./Bracket.jsx";
 import NoInfo from "./NoInfo.tsx";
 
 export default function TournamentResults({ matches, stage, isHost }) {
-    const matchesExist = matches.length > 0;
-    let groupedMatches = [];
+  const matchesExist = matches.length > 0;
+  let groupedMatches = [];
 
-    if (matchesExist) {
-        const validMatches = matches.filter(match => !match.qualifyingMatch);
-        const groupedMatchesObj = validMatches.reduce((acc, match) => {
-            const category = match.category.name;
-            
-            if (!acc[category]) {
-                acc[category] = [];
-            }
-            
-            acc[category].push(match);
-            return acc;
-        }, {});
-        groupedMatches = Object.values(groupedMatchesObj);
-    }
+  if (matchesExist) {
+    const validMatches = matches.filter((match) => !match.qualifyingMatch);
+    const groupedMatchesObj = validMatches.reduce((acc, match) => {
+      const category = match.category.name;
 
-    const canSeeBracket = (isHost && stage === "draw") || (stage === "play" || stage === "finished");
+      if (!acc[category]) {
+        acc[category] = [];
+      }
 
-    return (
-        <div className="standard-container container-indigo flex flex-col gap-2.5 z-0">
-            <h3>Tournament Results</h3>
-            <div className="flex flex-col items-center gap-2.5">
-                { canSeeBracket ? (
-                    <>
-                        { matchesExist ? (
-                            <>
-                                { groupedMatches.map(item => (
-                                    <Bracket matchData={item} categoryName={item[0].category.name} key={item[0].category.name} />
-                                ))}
-                            </>
-                        ) : (
-                            <div className="flex flex-col justify-center items-center gap-8">
-                                <div className="racket-cross-wrapper">
-                                    <img src="/assets/images/racket-red.svg" alt="" />
-                                    <img src="/assets/images/racket-blue.svg" alt="" />
-                                </div>
-                                <p>Matches for this tournament haven't been created yet!</p>
-                            </div>
-                        )}
-                    </>
-                ) : (
-                    <NoInfo text="Matches for this tournament haven't been created yet!" />
-                )}
-            </div>
-        </div>
-    )
+      acc[category].push(match);
+      return acc;
+    }, {});
+    groupedMatches = Object.values(groupedMatchesObj);
+  }
+
+  const canSeeBracket =
+    (isHost && stage === "draw") || stage === "play" || stage === "finished";
+
+  return (
+    <div className="standard-container container-indigo flex flex-col gap-2.5 z-0">
+      <h3>Tournament Results</h3>
+      <div className="flex flex-col items-center gap-2.5">
+        {canSeeBracket ? (
+          <>
+            {matchesExist ? (
+              <>
+                {groupedMatches.map((item) => (
+                  <Bracket
+                    matchData={item}
+                    categoryName={item[0].category.name}
+                    key={item[0].category.name}
+                  />
+                ))}
+              </>
+            ) : (
+              <div className="flex flex-col justify-center items-center gap-8">
+                <div className="racket-cross-wrapper">
+                  <img src="/assets/images/racket-red.svg" alt="" />
+                  <img src="/assets/images/racket-blue.svg" alt="" />
+                </div>
+                <p>Matches for this tournament haven't been created yet!</p>
+              </div>
+            )}
+          </>
+        ) : (
+          <NoInfo text="Matches for this tournament haven't been created yet!" />
+        )}
+      </div>
+    </div>
+  );
 }
